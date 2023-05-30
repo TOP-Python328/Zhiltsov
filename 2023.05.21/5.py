e120a6aa1ce7e1d330db6cd2dd7b71b5bf8858d8
Zhiltsov Alexander <127606998+ZhiltsovAlex@users.noreply.github.com>
2023-05-30 23:19:35 +0300

    Add files via upload

---
+++ 2023.05.21/5.py
@@ -0,0 +1,9 @@
+miles_integer, miles_fraction = int(input("")),int(input(""))
+miles = float(f'{miles_integer}.{miles_fraction}')
+kilometers = miles * 1.61
+kilometers = round(kilometers, 1)
+print(f"{miles} миль = {kilometers} км")
+
+           #65
+           #6
+           #65.6 миль = 105.6 км
