f6e303ec7c214f0102b6de0f177efb56a1ec22cf
Gennadiy <renardes@gmail.com>
2023-06-01 15:29:42 +0500

    2023.05.21: комментарии, правки, исправления

--- 2023.05.21/5.py
+++ 2023.05.21/5.py
@@ -1,9 +1,14 @@
-miles_integer, miles_fraction = int(input("")),int(input(""))
+miles_integer, miles_fraction = int(input("")), int(input(""))
 miles = float(f'{miles_integer}.{miles_fraction}')
 kilometers = miles * 1.61
+# КОММЕНТАРИЙ: если результат округления должен быть использован где-то ещё, тогда используете функцию round(); в данном случае выгоднее воспользоваться синтаксисом f-строк
 kilometers = round(kilometers, 1)
 print(f"{miles} миль = {kilometers} км")
 
-           #65
-           #6
-           #65.6 миль = 105.6 км
+
+# 65
+# 6
+# 65.6 миль = 105.6 км
+
+
+# ИТОГ: очень хорошо — 4/5
